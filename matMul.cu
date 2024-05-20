2a27348e4899f290114ad82cc5a82687399937d0
Vish Chaudhary <97370632+VishChaudhary@users.noreply.github.com>
2024-05-19 20:29:32 -0400

    Update matMul.cu

--- matMul.cu
+++ matMul.cu
@@ -1,4 +1,5 @@
-#define TILE_WIDTH 16
+#define TILE_WIDTH 32
+#define COARSE_FACTOR 4
 
 __global__ void matrixMulKernel (float* M, float * N, float* P, int width){
   __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
@@ -9,11 +10,15 @@ __global__ void matrixMulKernel (float* M, float * N, float* P, int width){
 
   // Identify the row and column of the P element to work on
   int row = by * TILE_WIDTH + ty;
-  int col = bx * TILE_WIDTH + tx;
+  int colStart = bx * TILE_WIDTH*COARSE_FACTOR + tx;
+
+  //Initialize Pvalue for all output elements
+  float Pvalue[COARSE_FACTOR];
+  for(int c=0; c<COARSE_FACTOR; ++c){
+    Pvalue[c] = 0.0f;
+  }
 
   //Loop over the M and N tiles required to compute P element
-  float Pvalue = 0;
-
   for( int ph = 0; ph<(width/(float)TILE_WIDTH); ++ph){
     if ((row < width) && (ph*TILE_WIDTH) < width){
       Mds[ty][tx] = M[row*width + ph*TILE_WIDTH +tx];
@@ -21,17 +26,21 @@ __global__ void matrixMulKernel (float* M, float * N, float* P, int width){
     else {
       Mds[ty][tx] = 0.0f;
     }
-
-    if ((ph*TILE_WIDTH+ty) < width && col < width){
-      Nds[ty][tx] = N[(ph*TILE_WIDTH +ty)*width + col];
+    for(int c=0; c<COARSE_FACTOR; ++C){
+      int col = colStart + c*TILE_WIDTH;
+      
+      //collaborative loading of N tile into shared memory
+      if ((ph*TILE_WIDTH+ty) < width && col < width){
+        Nds[ty][tx] = N[(ph*TILE_WIDTH +ty)*width + col];
+      }
+      else Nds[ty][tx] = 0.0f;
+      __syncthreads();
+      for (int k = 0; k < TILE_WIDTH; ++k){
+        Pvalue += Mds[ty][k] * Nds[k][tx];
+      }
+      __syncthreads();
     }
-    else Nds[ty][tx] = 0.0f;
-    __syncthreads();
-
-    for (int k = 0; k < TILE_WIDTH; ++k){
-      Pvalue += Mds[ty][k] * Nds[k][tx];
-    }
-    __syncthreads();
+    
   }
   if((row<width) && (col<width)){
     P[row*width+col] = Pvalue;
